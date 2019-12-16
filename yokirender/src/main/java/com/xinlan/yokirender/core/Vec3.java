0335ecddce4c6181659178b1c0cfbcd6bf0641b4
yixin.panyi <panyi@yixin.im>
2019-12-16 20:52:55 +0800

    primitive for point

---
+++ yokirender/src/main/java/com/xinlan/yokirender/core/Vec3.java
@@ -0,0 +1,26 @@
+package com.xinlan.yokirender.core;
+
+public final class Vec3 {
+    public float x;
+    public float y;
+    public float z;
+
+    public Vec3() {
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+    }
+
+    public Vec3(float x, float y, float z, float w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+    public Vec3(float[] data) {
+        this.x = data[0];
+        this.y = data[1];
+        this.z = data[2];
+    }
+}// end class
